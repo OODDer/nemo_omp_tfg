10e4791a8385605d81dd194410c1f5d5351d74b1
Oriol Duran <oriol.duran@bsc.es>
2023-01-12 13:30:01 +0100

    made debug messages cleaner

--- cfgs/ORCA2_OMP_COMMS/BLD/ppsrc/nemo/lbclnk.f90
+++ cfgs/ORCA2_OMP_COMMS/BLD/ppsrc/nemo/lbclnk.f90
@@ -1689,7 +1689,7 @@ CONTAINS
          IF( .NOT. ALLOCATED(buffrcv_dpp) )   ALLOCATE( buffrcv_dpp(iszR) )
          ! default definition when no communication is done. understood by mpi_waitall
          nreq_p2pp(:) = MPI_REQUEST_NULL   ! WARNING: Must be done after the call to mpi_waitall just above
-         WRITE(*,*)'tra_adv_fct comms:  sndbuffpp = ',SHAPE(buffsnd_dpp),'  recvbuffpp= ',SHAPE(buffrcv_dpp), ' from rank ',mpprank
+         WRITE(*,*)'Spp= ',SHAPE(buffsnd_dpp),'  Rpp= ',SHAPE(buffrcv_dpp), ' rank ',mpprank
          
       ELSE
          IF( ALLOCATED(buffsnd_dp) ) THEN
@@ -1704,9 +1704,7 @@ CONTAINS
          ! default definition when no communication is done. understood by mpi_waitall
          nreq_p2p(:) = MPI_REQUEST_NULL   ! WARNING: Must be done after the call to mpi_waitall just above
          IF(present(pTag))THEN
-            WRITE(*,*)'tra_adv_fct comms:  sndbuffp = ',SHAPE(buffsnd_dp),'  recvbuffp= ',SHAPE(buffrcv_dp), ' from rank ',mpprank
-         ELSE
-            WRITE(*,*)'reg comms:          sndbuff = ',SHAPE(buffsnd_dp),'  recvbuff= ',SHAPE(buffrcv_dp)
+            WRITE(*,*)'Sp= ',SHAPE(buffsnd_dp),'  Rp= ',SHAPE(buffrcv_dp), ' rank ',mpprank
          ENDIF
       ENDIF
       
