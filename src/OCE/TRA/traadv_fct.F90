4662452619de49979240db6580cdcb4a140a9247
Oriol Duran <oriol.duran@bsc.es>
2023-02-20 14:37:13 +0100

    comment l_ptr to test memory

--- src/OCE/TRA/traadv_fct.F90
+++ src/OCE/TRA/traadv_fct.F90
@@ -114,7 +114,7 @@ CONTAINS
          l_ptr = .FALSE.
          ll_zAimp = .FALSE.
          IF( ( cdtype == 'TRA' .AND. l_trdtra  ) .OR. ( cdtype =='TRC' .AND. l_trdtrc ) )      l_trd = .TRUE.
-         IF(   cdtype == 'TRA' .AND. ( iom_use( 'sophtadv' ) .OR. iom_use( 'sophtadv' ) ) )    l_ptr = .TRUE.
+         !IF(   cdtype == 'TRA' .AND. ( iom_use( 'sophtadv' ) .OR. iom_use( 'sophtadv' ) ) )    l_ptr = .TRUE.
          IF(   cdtype == 'TRA' .AND. ( iom_use("uadv_heattr") .OR. iom_use("vadv_heattr") .OR.  &
             &                         iom_use("uadv_salttr") .OR. iom_use("vadv_salttr")  ) )  l_hst = .TRUE.
          !
